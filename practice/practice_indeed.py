0073482d35471b704f0b722bb4a2f43f3bd997d1
youjin lee <youjinlee1997@gmail.com>
2020-08-23 11:56:16 +0900

    2.7 extracting companies

--- practice/practice_indeed.py
+++ practice/practice_indeed.py
@@ -5,10 +5,10 @@ URL = "https://www.indeed.com/jobs?q=python&limit=50"
 LIMIT = 50
 
 def extract_indeed_page():
-    indeed_result = requests.get(URL)
+    result = requests.get(URL)
 
-    indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
-    pagination = indeed_soup.find("div", "pagination")
+    soup = BeautifulSoup(result.text, 'html.parser')
+    pagination = soup.find("div", "pagination")
 
     links = pagination.find_all("a")
 
@@ -25,5 +25,9 @@ def extract_indeed_jobs(last_page):
     jobs = []
     for page in range(last_page):
         result = requests.get(f"{URL}&start={page*LIMIT}")
-
+        soup = BeautifulSoup(result.text, 'html.parser')
+        results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        for result in results:
+            title = result.find("h2", {"class": "title"}).find("a")["title"]
+            print(title)
     return jobs

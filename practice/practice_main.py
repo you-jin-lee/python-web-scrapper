4c2105a2125cca66b1ea12d3b25d22ad5f67bb8f
youjin lee <youjinlee1997@gmail.com>
2020-08-28 22:47:41 +0900

    2.14 what is csv

--- practice/practice_main.py
+++ practice/practice_main.py
@@ -1,7 +1,10 @@
 from practice_indeed import get_jobs as get_indeed_jobs
 from practice_so import get_jobs as get_so_jobs
+from save import save_to_file
 
 indeed_jobs = get_indeed_jobs()
 so_jobs = get_so_jobs()
 
 jobs = indeed_jobs + so_jobs
+
+save_to_file(jobs)
